2740e9dbbc19e48c5a16fcabae893ca8288c32ae
Max Sprauer <max.sprauer@skyspecs.com>
2022-12-02 22:11:58 -0500

    First two days

---
+++ 01_calorie_counting/calories.ts
@@ -0,0 +1,15 @@
+import * as fs from "fs";
+
+process.chdir(__dirname);
+var lines = fs.readFileSync("input.txt", "utf8");
+
+const calGroups = lines.split("\n\n");
+const cals = calGroups.map((group) => group.split("\n").map((cal) => +cal)); // Convert string to int
+
+const calSums = cals.map((group) => group.reduce((prev, cur) => prev + cur, 0));
+calSums.sort((a, b) => b - a);
+
+console.log("Part one: " + calSums[0]);
+console.log(
+  "Part two: " + calSums.splice(0, 3).reduce((prev, cur) => prev + cur, 0)
+);
---
+++ 02_rock_paper_scissors/rock.ts
@@ -0,0 +1,66 @@
+import * as fs from "fs";
+import { exit } from "process";
+
+process.chdir(__dirname);
+var lines = fs.readFileSync("input.txt", "utf8").trim().split("\n");
+
+// Dumb, but fast
+const scores = {
+  "A X": 3,
+  "A Y": 6,
+  "A Z": 0,
+  "B X": 0,
+  "B Y": 3,
+  "B Z": 6,
+  "C X": 6,
+  "C Y": 0,
+  "C Z": 3,
+};
+
+const values = {
+  X: 1,
+  Y: 2,
+  Z: 3,
+};
+
+const partOne = lines.reduce(
+  (prev, line) => prev + scores[line] + values[line[2]],
+  0
+);
+console.log(`Part One: ${partOne}`);
+
+const partTwo = lines.reduce((prev, line) => {
+  let myChoice;
+  let theirChoice = line[0];
+  switch (line[2]) {
+    case "X":
+      // Lose
+      for (const [key, value] of Object.entries(scores)) {
+        if (key[0] === theirChoice && value === 0) {
+          myChoice = key[2];
+          break;
+        }
+      }
+      break;
+
+    case "Y":
+      // Draw
+      myChoice = String.fromCharCode(
+        "X".charCodeAt(0) + (theirChoice.charCodeAt(0) - "A".charCodeAt(0))
+      );
+      break;
+
+    case "Z":
+      // Win
+      for (const [key, value] of Object.entries(scores)) {
+        if (key[0] === theirChoice && value === 6) {
+          myChoice = key[2];
+          break;
+        }
+      }
+      break;
+  }
+  console.log(`${theirChoice} ${myChoice}`);
+  return prev + scores[`${theirChoice} ${myChoice}`] + values[myChoice];
+}, 0);
+console.log(`Part Two: ${partTwo}`);
